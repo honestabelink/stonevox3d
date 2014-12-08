cb8cb31fc7599b9cf72e4c32b6e9316b64a2e6f4
honestabelink <honestabelink@hotmail.com>
2014-12-08 15:54:03 -0500

    color data for voxels
    mainly to enforce alpha masks, slick's color class enforces 0.0 to 1.0,
    so I needed to use another color class anyway... why not use your own.

---
+++ src/stonevox/data/Color.java
@@ -0,0 +1,17 @@
+package stonevox.data;
+
+public class Color
+{
+	public float r;
+	public float g;
+	public float b;
+	public int a;
+
+	public Color(float r, float g, float b, int a)
+	{
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+}
