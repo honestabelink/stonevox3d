bf79f38172c10212f98f423a4d7e4b1b2d4b3d64
honestabelink <honestabelink@hotmail.com>
2015-08-24 01:36:49 -0400

    Helper classes for voxel volume editing

---
+++ stonevox/qbtypes/VoxelColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stonevox
+{
+    public struct VoxelColor
+    {
+        public int colorindex;
+
+        public VoxelColor(int colorindex)
+        {
+            this.colorindex = colorindex;
+        }
+    }
+}
