424e26b44323702485a57904a27b9f4e6ddb95c2
honestabelink <honestabelink@hotmail.com>
2015-08-26 20:03:32 -0400

    helper function to resize bitmaps

--- stonevox/utils/Extensions.cs
+++ stonevox/utils/Extensions.cs
@@ -132,5 +132,24 @@ namespace stonevox
         {
             return Scale.vUnSizeScale(value);
         }
+
+        public static Bitmap ResizeImage(this Bitmap imgToResize, Size size)
+        {
+            try
+            {
+                Bitmap b = new Bitmap(size.Width, size.Height);
+                using (Graphics g = Graphics.FromImage((Image)b))
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
+                }
+                return b;
+            }
+            catch
+            {
+                Console.WriteLine("Bitmap could not be resized");
+                return imgToResize;
+            }
+        }
     }
 }
