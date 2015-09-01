27fa7165c3680f25c1264909559a5c87951e9261
honestabelink <honestabelink@hotmail.com>
2015-08-31 23:08:42 -0400

    what will be the matrix list box

---
+++ stonevox/gui/widgets/QbModelMatrixListbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stonevox
+{
+    public class QbModelMatrixListbox : Widget
+    {
+        public QbModelMatrixListbox(float width, float height)
+            : base(-1)
+        {
+            ID = GetNextAvailableID();
+        }
+    }
+}
