805fa847726e0f3afef84962ea12fa529b1e048c
honestabelink <honestabelink@hotmail.com>
2015-08-31 23:11:18 -0400

    build incrementing

--- stonevox/Properties/AssemblyInfo1.cs
+++ stonevox/Properties/AssemblyInfo1.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.0.1161")]
-[assembly: AssemblyFileVersion("0.0.0.1161")]
+[assembly: AssemblyVersion("0.0.0.1244")]
+[assembly: AssemblyFileVersion("0.0.0.1244")]
 
