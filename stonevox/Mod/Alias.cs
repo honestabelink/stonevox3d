66997b1813ea2e9333de3039ac50a46438d135b6
honestabelink <honestabelink@hotmail.com>
2015-10-25 18:48:45 -0400

    Starting work for modding support... i cut the work short on this, i'm looking to re-release Stonevox to the StoneHearth community. so this is on the back burner again for a bit

---
+++ stonevox/Mod/Alias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stonevox.Mod
+{
+    public class Alias
+    {
+        public string name;
+        public FileReference file;
+    }
+}
